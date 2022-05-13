4a10be2c77cabd7b4e3fe1cb225f1c25e03456fa
ivan8m8 <ivan8m8@gmail.com>
2022-05-13 23:50:27 +0300

    started to implement MainActivity & implemented its layout

--- app/src/main/java/io/github/ivan8m8/courierhelper/MainActivity.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/MainActivity.kt
@@ -2,10 +2,24 @@ package io.github.ivan8m8.courierhelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.ivan8m8.courierhelper.databinding.ActivityMainBinding
+import io.github.ivan8m8.courierhelper.ui.DeliveriesOsmMapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.mainContainer, DeliveriesOsmMapFragment.newInstance())
+                .commit()
+        }
     }
 }
