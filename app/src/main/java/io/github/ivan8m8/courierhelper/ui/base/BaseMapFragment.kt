1eea961b703810bbaf87511daec8a9a185466664
ivan8m8 <ivan8m8@gmail.com>
2022-05-13 19:13:56 +0300

    added the osmdroid dependence as well as all the required permissions by OSM & implemented BaseMapFragment

---
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/base/BaseMapFragment.kt
@@ -0,0 +1,24 @@
+package io.github.ivan8m8.courierhelper.ui.base
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+
+abstract class BaseMapFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestLocationPermissions()
+    }
+
+    private fun requestLocationPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){}
+            .launch(permissions)
+    }
+}
