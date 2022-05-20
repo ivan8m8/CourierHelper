efb96bf7cb66b09630a9982ebcec05d206f5f376
ivan8m8 <ivan8m8@gmail.com>
2022-05-20 22:13:55 +0300

    implemented BaseTopFragment

---
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/base/BaseTopFragment.kt
@@ -0,0 +1,22 @@
+package io.github.ivan8m8.courierhelper.ui.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+/**
+ * If a regular Fragment lays on top of another one (e.g. committed via transaction.add()),
+ * the behind one receives the unconsumed touch events of the top one.
+ * BaseTopFragment prevents this issue by consuming all the touch events.
+ */
+abstract class BaseTopFragment: Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+            isClickable = true
+            isFocusable = true
+        }
+    }
+}
