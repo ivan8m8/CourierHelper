7aec8f4fdc2634a9e022b2f4c6cd5af16d60539a
ivan8m8 <ivan8m8@gmail.com>
2022-11-01 00:11:36 +0300

    implemented doOnRealUserInput

--- app/src/main/java/io/github/ivan8m8/courierhelper/ui/custom/AutocompleteTextInputEditText.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/custom/AutocompleteTextInputEditText.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.appcompat.widget.ListPopupWindow
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
 import io.github.ivan8m8.courierhelper.R
 
@@ -17,29 +18,44 @@ class AutocompleteTextInputEditText(
     constructor(context: Context): this(context, null)
     constructor(context: Context, attrs: AttributeSet?): this(context, attrs, com.google.android.material.R.attr.editTextStyle)
 
-    private var shouldShowPopup = false //todo: comment
+    // Indicates that the last user action was a suggestion click,
+    // so we do not have to show the suggestion list. As well
+    // as we do not want to send a server request.
+    var isLastActionSuggestionClick = false
     private val popupAdapter = ArrayAdapter<String>(context, R.layout.item_autocomplete)
     private val popupWindow = ListPopupWindow(context).apply {
         anchorView = this@AutocompleteTextInputEditText.rootView
         setAdapter(this@AutocompleteTextInputEditText.popupAdapter)
         setOnItemClickListener { _, view, _, _ ->
+            isLastActionSuggestionClick = true
             with(this@AutocompleteTextInputEditText) {
                 setText((view as TextView).text)
                 setSelection(length())
             }
             dismiss()
-            shouldShowPopup = false //todo: have to send outside that a server request shouldn't be performed
         }
     }
     var items: List<String> = ArrayList()
         set(value) {
-            if (!shouldShowPopup) {
-                shouldShowPopup = true
-                return
-            }
             popupAdapter.clear()
             popupAdapter.addAll(value)
             //todo: possibly, we have to update suggestions over calling show()
             popupWindow.show()
         }
+
+    /**
+     * Same as `doOnTextChanged`, but in contrast this ignores
+     * the fake event when the user taps on a suggestion.
+     */
+    inline fun doOnRealUserInput(
+        crossinline action: (String) -> Unit
+    ) = addTextChangedListener(
+        afterTextChanged = { editable ->
+            if (!isLastActionSuggestionClick) {
+                val userInput = editable?.toString() ?: return@addTextChangedListener
+                action.invoke(userInput)
+            } else
+                isLastActionSuggestionClick = false
+        }
+    )
 }
