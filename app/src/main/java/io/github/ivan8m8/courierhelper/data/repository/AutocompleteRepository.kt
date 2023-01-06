8a18ac4503360a5ce06316f1f509604c3281b532
ivan8m8 <ivan8m8@gmail.com>
2023-01-06 18:51:40 +0300

    updated AutocompleteRepository

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/repository/AutocompleteRepository.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/repository/AutocompleteRepository.kt
@@ -1,15 +1,40 @@
 package io.github.ivan8m8.courierhelper.data.repository
 
+import io.github.ivan8m8.courierhelper.data.models.KladrModels.Address
+import io.github.ivan8m8.courierhelper.data.models.KladrModels.Region
+import io.github.ivan8m8.courierhelper.data.models.KladrModels.Suggestion
 import io.github.ivan8m8.courierhelper.data.network.KladrApi
 import io.reactivex.rxjava3.core.Single
 
 class AutocompleteRepository(
     private val kladrApi: KladrApi
 ) {
-    fun autocompleteAddress(query: String): Single<List<String>> {
-        return kladrApi.queryOneString(query)
+
+    fun autocompleteRegion(
+        query: String
+    ): Single<List<Region>> {
+        return kladrApi.autocompleteRegion(query)
             .map { response ->
-                response.result?.map { it.fullName } ?: emptyList()
+                response.result ?: emptyList()
             }
+            .excludeWatermark()
     }
+
+    fun autocompleteAddress(
+        query: String,
+        regionId: String
+    ): Single<List<Address>> {
+        return kladrApi.autocompleteAddress(query, regionId)
+            .map { response ->
+                response.result ?: emptyList()
+            }
+            .excludeWatermark()
+    }
+
+    private fun <T : Suggestion> Single<List<T>>.excludeWatermark() = this
+        .map { list ->
+            list.filter { item ->
+                item.id != "Free"
+            }
+        }
 }
