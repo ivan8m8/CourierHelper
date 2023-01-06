94ff2a13ea2fc608a374f906be6ffe68c40df0b6
ivan8m8 <ivan8m8@gmail.com>
2023-01-06 15:08:46 +0300

    updated MainModule

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/di/MainModule.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/di/MainModule.kt
@@ -2,7 +2,11 @@ package io.github.ivan8m8.courierhelper.data.di
 
 import androidx.room.Room
 import io.github.ivan8m8.courierhelper.data.db.AppDb
+import io.github.ivan8m8.courierhelper.data.repository.AutocompleteRepository
+import io.github.ivan8m8.courierhelper.data.repository.DeliveriesRepository
+import io.github.ivan8m8.courierhelper.ui.viewmodels.AddDeliveryViewModel
 import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object MainModule {
@@ -18,6 +22,15 @@ object MainModule {
         }
         single {
             get<AppDb>().deliveriesDao()
+        }
+        single {
+            DeliveriesRepository(get())
+        }
+        single {
+            AutocompleteRepository(get())
+        }
+        viewModel {
+            AddDeliveryViewModel(get(), get())
         }
     }
 }
