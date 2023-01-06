9097b4dbc7d2be8a21620c7482de32906beb4d4f
ivan8m8 <ivan8m8@gmail.com>
2023-01-06 14:51:37 +0300

    updated KladrApi

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/network/KladrApi.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/network/KladrApi.kt
@@ -1,18 +1,27 @@
 package io.github.ivan8m8.courierhelper.data.network
 
+import io.github.ivan8m8.courierhelper.data.models.KladrModels.AddressResponse
+import io.github.ivan8m8.courierhelper.data.models.KladrModels.RegionResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface KladrApi {
-    @GET("api.php?oneString=1")
-    fun queryOneString(
+
+    @GET("api.php?contentType=region")
+    fun autocompleteRegion(
         @Query("query") query: String,
         @Query("limit") limit: Int = 5
-    ): Single<QueryResponse>
+    ): Single<RegionResponse>
+
+    @GET("api.php?oneString=1")
+    fun autocompleteAddress(
+        @Query("query") query: String,
+        @Query("regionId") regionId: String,
+        @Query("limit") limit: Int = 5
+    ): Single<AddressResponse>
+
     companion object {
         const val BASE_URL = "https://kladr-api.ru/"
-        data class QueryResponse(val result: List<Result>?)
-        data class Result(val fullName: String)
     }
 }
