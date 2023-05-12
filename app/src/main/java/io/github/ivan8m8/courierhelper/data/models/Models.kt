957bb3e6d91d7606a14d02495d98d0a44d3744a0
ivan8m8 <ivan8m8@gmail.com>
2023-05-13 01:19:30 +0300

    fixed a bug with serialization

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/models/Models.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/models/Models.kt
@@ -1,9 +1,11 @@
 package io.github.ivan8m8.courierhelper.data.models
 
+import com.google.gson.annotations.SerializedName
+
 object Models {
     data class LatitudeLongitude(
         val lat: Double,
-        val lng: Double
+        @SerializedName("lon") val lng: Double
     )
     enum class DeliveryStatus {
         IN_PROGRESS,
