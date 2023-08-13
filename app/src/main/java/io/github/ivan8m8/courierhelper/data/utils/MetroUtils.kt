da152c424314f969d7f35aeca9c584baaa4c41b1
ivan8m8 <ivan8m8@gmail.com>
2023-08-14 00:14:53 +0300

    started to implement MetroUtils

---
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/utils/MetroUtils.kt
@@ -0,0 +1,50 @@
+package io.github.ivan8m8.courierhelper.data.utils
+
+import io.github.ivan8m8.courierhelper.data.models.Models.LatitudeLongitude
+import kotlin.math.cos
+import kotlin.math.sin
+
+//https://www.frameworks.su/article/rasstoyanie_do_bligayshih_stantsii_metro
+class MetroUtils {
+
+    fun getClosestArea(
+        latLng: LatitudeLongitude,
+        distanceM: Int = 2000
+    ): ClosestArea {
+        val east = getAreaPoint(latLng, 0.0, distanceM)
+        val north = getAreaPoint(latLng, 90.0, distanceM)
+        val west = getAreaPoint(latLng, 180.0, distanceM)
+        val south = getAreaPoint(latLng, 270.0, distanceM)
+        return ClosestArea(west, north, east, south)
+    }
+
+    private fun getAreaPoint(
+        latLng: LatitudeLongitude,
+        angleDegree: Double,
+        distanceM: Int
+    ): LatitudeLongitude {
+        val angleRad = Math.toRadians(angleDegree)
+        val latRad = Math.toRadians(latLng.lat)
+        val dx = distanceM / (LAT_1_DEGREE_LENGTH * cos(latRad)) * cos(angleRad)
+        val dy = distanceM / LAT_1_DEGREE_LENGTH * sin(angleRad)
+        return LatitudeLongitude(
+            latLng.lat + dy,
+            latLng.lng + dx
+        )
+    }
+
+    companion object {
+
+        data class ClosestArea(
+            val west: LatitudeLongitude,
+            val north: LatitudeLongitude,
+            val east: LatitudeLongitude,
+            val south: LatitudeLongitude
+        )
+
+        /**
+         * Meridian length of 1 degree of latitude on the sphere in meters.
+         */
+        private const val LAT_1_DEGREE_LENGTH: Double = 40075 * 1000 / 360.0
+    }
+}
