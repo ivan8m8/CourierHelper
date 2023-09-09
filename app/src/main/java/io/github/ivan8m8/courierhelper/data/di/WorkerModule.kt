253ee3f5b5450840c185f5a3ba00d005019978c6
ivan8m8 <ivan8m8@gmail.com>
2023-09-09 22:28:00 +0300

    injected WorkManager

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/di/WorkerModule.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/di/WorkerModule.kt
@@ -1,6 +1,8 @@
 package io.github.ivan8m8.courierhelper.data.di
 
+import androidx.work.WorkManager
 import io.github.ivan8m8.courierhelper.data.workers.FetchMetroStationsWorker
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.dsl.worker
 import org.koin.dsl.module
 
@@ -12,6 +14,9 @@ object WorkerModule {
                 get(),
                 get()
             )
+        }
+        single {
+            WorkManager.getInstance(androidContext())
         }
     }
 }
