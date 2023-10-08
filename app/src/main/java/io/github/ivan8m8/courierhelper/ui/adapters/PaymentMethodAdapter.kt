2438f369a662c9cf6ded0a757948e3f3acbc3059
ivan8m8 <ivan8m8@gmail.com>
2023-10-08 16:40:22 +0300

    updated PaymentMethodAdapter

--- app/src/main/java/io/github/ivan8m8/courierhelper/ui/adapters/PaymentMethodAdapter.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/adapters/PaymentMethodAdapter.kt
@@ -9,20 +9,28 @@ import android.widget.TextView
 import io.github.ivan8m8.courierhelper.R
 import io.github.ivan8m8.courierhelper.ui.models.UiModels.UiPaymentMethod
 
-class PaymentMethodAdapter(context: Context) : ArrayAdapter<UiPaymentMethod>(
+class PaymentMethodAdapter(
+    context: Context,
+    private val onItemClick: (UiPaymentMethod?) -> Unit
+) : ArrayAdapter<UiPaymentMethod>(
     context,
     R.layout.item_autocomplete_payment_method
 ) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         return convertView ?: run {
-            val v = LayoutInflater
-                .from(context)
-                .inflate(R.layout.item_autocomplete_payment_method, parent, false) as TextView
             val item = getItem(position)
-            v.text = item?.text ?: ""
-            v.setCompoundDrawablesWithIntrinsicBounds(item?.icon, null, null, null)
-            v
+            LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_autocomplete_payment_method, parent, false)
+                .let { it as TextView }
+                .apply {
+                    text = item?.text ?: ""
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(item?.icon, null, null, null)
+                    setOnClickListener {
+                        onItemClick(item)
+                    }
+                }
         }
     }
 }
--- app/src/main/java/io/github/ivan8m8/courierhelper/ui/fragments/AddDeliveryFragment.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/fragments/AddDeliveryFragment.kt
@@ -1,5 +1,7 @@
 package io.github.ivan8m8.courierhelper.ui.fragments
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
@@ -7,6 +9,7 @@ import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import io.github.ivan8m8.courierhelper.R
 import io.github.ivan8m8.courierhelper.databinding.FragmentAddDeliveryBinding
+import io.github.ivan8m8.courierhelper.ui.adapters.PaymentMethodAdapter
 import io.github.ivan8m8.courierhelper.ui.utils.addSystemBottomMargin
 import io.github.ivan8m8.courierhelper.ui.utils.addSystemBottomPadding
 import io.github.ivan8m8.courierhelper.ui.utils.addSystemTopMargin
@@ -21,6 +24,13 @@ class AddDeliveryFragment: BaseTopFragment(R.layout.fragment_add_delivery) {
 
     private val binding by viewBinding(FragmentAddDeliveryBinding::bind)
     private val viewModel: AddDeliveryViewModel by viewModel()
+    private val paymentMethodArrayAdapter by lazy {
+        PaymentMethodAdapter(requireContext()) { paymentMethod ->
+            binding.paymentMethodAutoComplete.dismissDropDown()
+            binding.paymentMethodAutoComplete.setText(paymentMethod?.text, false)
+            binding.paymentMethodInputLayout.startIconDrawable = paymentMethod?.icon
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,6 +48,10 @@ class AddDeliveryFragment: BaseTopFragment(R.layout.fragment_add_delivery) {
             orderNumberEditText.doAfterTextChanged { text -> viewModel.orderNumberChanged(text?.toString()) }
             itemPriceEditText.doAfterTextChanged { text -> viewModel.itemPriceChanged(text?.toString()) }
             itemNameEditText.doAfterTextChanged { text -> viewModel.itemNameChanged(text?.toString()) }
+            with(paymentMethodAutoComplete) {
+                setAdapter(paymentMethodArrayAdapter)
+                setDropDownBackgroundDrawable(ColorDrawable(Color.WHITE))
+            }
             clientNameEditText.doAfterTextChanged { text -> viewModel.clientNameChanged(text?.toString()) }
             commentEditText.doAfterTextChanged { text -> viewModel.commentChanged(text?.toString()) }
             addButton.setOnClickListener { viewModel.addDeliveryClicked() }
@@ -48,6 +62,10 @@ class AddDeliveryFragment: BaseTopFragment(R.layout.fragment_add_delivery) {
             }
             addressErrorTextLiveData.observe(viewLifecycleOwner) { error ->
                 binding.addressEditText.error = error
+            }
+            paymentMethodsLiveData.observe(viewLifecycleOwner) { paymentMethods ->
+                paymentMethodArrayAdapter.clear()
+                paymentMethodArrayAdapter.addAll(paymentMethods)
             }
             errorsLiveData.observe(viewLifecycleOwner) { event ->
                 event.getContentIfNotHandled()?.let { message ->
