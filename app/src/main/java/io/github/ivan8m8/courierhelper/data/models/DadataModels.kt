2904cd744a7e5f2fe7d58d0e9c3168003642638a
ivan8m8 <ivan8m8@gmail.com>
2023-10-08 23:52:29 +0300

    added LocationBoost

--- app/src/main/java/io/github/ivan8m8/courierhelper/data/models/DadataModels.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/data/models/DadataModels.kt
@@ -4,8 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 object DadataModels {
 
+    data class LocationBoost(
+        @SerializedName("kladr_id") val id: String
+    )
+
     data class RequestBody(
         val query: String,
+        @SerializedName("locations_boost") val locationBoost: List<LocationBoost>? = null,
         @SerializedName("count") val limit: Int = 5
     )
 
