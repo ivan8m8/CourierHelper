0854775f2036b744a297cbf551cab88d9569199d
ivan8m8 <ivan8m8@gmail.com>
2024-08-26 02:25:07 +0300

    desired BaseModalBottomSheetFragment implementation via Theme

--- app/src/main/java/io/github/ivan8m8/courierhelper/ui/fragments/base/BaseModalBottomSheetFragment.kt
+++ app/src/main/java/io/github/ivan8m8/courierhelper/ui/fragments/base/BaseModalBottomSheetFragment.kt
@@ -7,23 +7,22 @@ import androidx.annotation.LayoutRes
 import androidx.annotation.Px
 import androidx.core.graphics.Insets
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import io.github.ivan8m8.courierhelper.R
 import io.github.ivan8m8.courierhelper.data.utils.lazyUnsafe
 import io.github.ivan8m8.courierhelper.ui.utils.doOnApplyWindowInsetsWithPaddings
 
 /**
- * Base class for a modal bottom sheet that always slides below the status bar.
+ * Base class for a modal bottom sheet that always slides below the status bar and
+ * never adds any unexpected bottom padding, allowing its content to be properly
+ * drawn and scrolled under the navigation bar.
  *
- * As well, this never adds any unwanted bottom padding, allowing its content
- * to be properly drawn and scrolled under the status bar. Although that can
- * be achieved by setting `paddingBottomSystemWindowInsets` to `false`.
- * And there's also an [alternative way](https://stackoverflow.com/a/68937978/7541231).
- *
- * For some reason, [BottomSheetBehavior] handles the bottom inset by adding it as a
- * bottom padding to its child when the sheet's top reaches the status bar, and this is
+ * For some reason, the [BottomSheetBehavior] in a regular [BottomSheetDialog]
+ * handles the bottom inset by adding it as a bottom padding to the sheet's
+ * container. So as soon as the sheet's top starts reaching the status bar,
+ * that unexpected bottom padding starts being visible. And this is
  * [considered intended behavior](https://github.com/material-components/material-components-android/issues/2596).
  */
 open class BaseModalBottomSheetFragment(
@@ -44,6 +43,8 @@ open class BaseModalBottomSheetFragment(
 
     private val bottomSheetBehavior by lazyUnsafe { (requireDialog() as BottomSheetDialog).behavior }
 
+    override fun getTheme() = R.style.ThemeOverlay_BaseModalBottomSheetDialog
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         return bottomSheetDialog.apply {
@@ -58,23 +59,15 @@ open class BaseModalBottomSheetFragment(
         super.onViewCreated(view, savedInstanceState)
 
         requireView().doOnApplyWindowInsetsWithPaddings { _, windowInsets, paddings ->
-
             val type = WindowInsetsCompat.Type.systemBars()
             val insetsRect = windowInsets.getInsets(type)
-
-            // Let's erase the inset bottom padding applied by BottomSheetBehavior
-            requireDialog().findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-                .updatePadding(bottom = 0)
-
-            // Let's prevent the sheet's top from reaching the status bar
             val expandedOffset = insetsRect.top * topInsetMultiplier + extraTopOffset + paddings.top
             bottomSheetBehavior.expandedOffset = expandedOffset
-
-            val bottomPadding = expandedOffset + insetsRect.bottom
+            val bottomInset = expandedOffset + insetsRect.bottom
             WindowInsetsCompat.Builder()
                 .setInsets(
                     type,
-                    Insets.of(insetsRect.left, 0, insetsRect.right, bottomPadding)
+                    Insets.of(insetsRect.left, 0, insetsRect.right, bottomInset)
                 )
                 .build()
         }
