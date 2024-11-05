4052520b43a9fa465cc34f682348ad4a3e042a38
ivan8m8 <ivan8m8@gmail.com>
2024-11-06 00:55:29 +0300

    Bump compile and target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "io.github.ivan8m8.courierhelper"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.ivan8m8.courierhelper"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
