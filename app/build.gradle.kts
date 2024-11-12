e8a72f3ec66726aaaae69a9032be57e94a025f94
ivan8m8 <ivan8m8@gmail.com>
2024-11-13 01:45:26 +0300

    Migrate to Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.secrets)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -46,46 +46,35 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation(libs.adapterdelegates)
+    implementation(libs.adapterdelegates.viewbinding)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.room)
+    ksp           (libs.androidx.room.compiler)
+    implementation(libs.androidx.room.rxjava3)
+    implementation(libs.androidx.workmanager)
+    implementation(libs.androidx.workmanager.rxjava3)
+    implementation(libs.koin)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.workmanager)
+    implementation(libs.material)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.osmdroid)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.retrofit.rxjava3.adapter)
+    implementation(libs.rxandroid)
+    implementation(libs.rxjava3)
+    implementation(libs.rxkotlin)
+    implementation(libs.rxrelay)
+    implementation(libs.timber)
 
-    implementation("org.osmdroid:osmdroid-android:6.1.11")
-    implementation("androidx.preference:preference-ktx:1.2.1")
+    debugImplementation(libs.leakcanary)
 
-    val workVersion = "2.10.0"
-    implementation("androidx.work:work-runtime-ktx:$workVersion")
-    implementation("androidx.work:work-rxjava3:$workVersion")
-
-    implementation("io.reactivex.rxjava3:rxjava:3.1.4")
-    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
-    implementation("com.jakewharton.rxrelay3:rxrelay:3.0.1")
-
-    val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-rxjava3:$roomVersion")
-
-    implementation("com.hannesdorfmann:adapterdelegates4-kotlin-dsl:4.3.2")
-    implementation("com.hannesdorfmann:adapterdelegates4-kotlin-dsl-viewbinding:4.3.2")
-
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.11")
-
-    val koinVersion = "3.4.3"
-    implementation("io.insert-koin:koin-core:$koinVersion")
-    implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-workmanager:$koinVersion")
-
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    implementation("com.jakewharton.timber:timber:5.0.1")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso)
 }
